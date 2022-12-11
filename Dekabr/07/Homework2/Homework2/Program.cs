24d409a4bc1a9dd9eb28dc9388a8c4c795f858e4
Murad Zakirzade <ctgameaze@gmail.com>
2022-12-11 12:56:23 +0400

    homework2

---
+++ Dekabr/07/Homework2/Homework2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Şəhər adı qeyd edin 1 :");
+            string city1 = Console.ReadLine();
+            Console.Write("Şəhər adı qeyd edin 2 :");
+            string city2 = Console.ReadLine();
+            Console.Write("Şəhər adı qeyd edin 3 :");
+            string city3 = Console.ReadLine();
+            Console.Write("Şəhər adı qeyd edin 4 :");
+            string city4 = Console.ReadLine();
+            Console.Write("Şəhər adı qeyd edin 5 :");
+            string city5 = Console.ReadLine();
+            Console.Write("Şəhər adı qeyd edin 6 :");
+            string city6 = Console.ReadLine();
+            Console.Write("Şəhər adı qeyd edin 7 :");
+            string city7 = Console.ReadLine();
+            Console.Write("Şəhər adı qeyd edin 8 :");
+            string city8 = Console.ReadLine();
+            Console.Write("Şəhər adı qeyd edin 9 :");
+            string city9 = Console.ReadLine();
+            Console.Write("Şəhər adı qeyd edin 10 :");
+            string city10 = Console.ReadLine();
+            Console.WriteLine(city1 + ',' + city2 + ',' + city3 + ',' + city4 + ',' + city5 + ',' + city6 + ',' + city7 + ',' + city8 + ',' + city9 + ',' + city10);
+            Console.ReadLine();
+        }
+    }
+}
