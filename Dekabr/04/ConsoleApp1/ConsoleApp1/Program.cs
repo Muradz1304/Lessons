f212f370e5f13643eb32188b12792ac3aecc5075
Murad Zakirzade <ctgameaze@gmail.com>
2022-12-11 12:17:12 +0400

    Homework1

---
+++ Dekabr/04/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Adınızı qeyd edin:");
+
+            string name = Console.ReadLine();
+
+            Console.Write("Soyadınızı qeyd edin:");
+
+            string surname = Console.ReadLine();
+
+            Console.Write("Yaşadığınız şəhəri qeyd edin:");
+
+            string city = Console.ReadLine();
+
+        
+
+        }
+    }
+}
