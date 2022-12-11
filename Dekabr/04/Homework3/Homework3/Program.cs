a38aa3b074b020401d9e2714f0e72e611b2d4efb
Murad Zakirzade <ctgameaze@gmail.com>
2022-12-11 13:05:32 +0400

    homework3

---
+++ Dekabr/04/Homework3/Homework3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Şəhər adı qeyd edin 1 :");
+            string city1 = Console.ReadLine();
+            Console.Write("Şəhər adı qeyd edin 2 :");
+            string city2 = Console.ReadLine(); 
+            Console.Write("Şəhər adı qeyd edin 3 :");
+            string city3 = Console.ReadLine();
+            Console.Write("Şəhər adı qeyd edin 4 :");
+            string city4 = Console.ReadLine();
+            Console.Write("Şəhər adı qeyd edin 5 :");
+            string city5 = Console.ReadLine();
+            Console.Write("Şəhər adı qeyd edin 6 :");
+            string city6 = Console.ReadLine();
+            Console.Write("Şəhər adı qeyd edin 7 :");
+            string city7 = Console.ReadLine();
+            Console.Write("Şəhər adı qeyd edin 8 :");
+            string city8 = Console.ReadLine();
+            Console.Write("Şəhər adı qeyd edin 9 :");
+            string city9 = Console.ReadLine();
+            Console.Write("Şəhər adı qeyd edin 10 :");
+            string city10 = Console.ReadLine();
+            Console.WriteLine(city1);
+            Console.WriteLine(city2);
+            Console.WriteLine(city3);
+            Console.WriteLine(city4);
+            Console.WriteLine(city5);
+            Console.WriteLine(city6);
+            Console.WriteLine(city7);
+            Console.WriteLine(city8);
+            Console.WriteLine(city9);
+            Console.WriteLine(city10);
+            Console.ReadLine();
+
+        }
+    }
+}
